d07b031e1deda97aba08165e017648d14e05a5ac
KonstancjaMG <konstancja.maciejewska-gruenig@hicoders.ch>
2023-11-11 00:18:30 +0100

    Added a createSeparator() function and appended it to the app element.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { renderNav } from "./navigation.js"
 import { LINKS } from "./data.js"
+import { createSeparator } from "./spacer.js"
 
 const MAIN_HTML = document.getElementById("app")
 
 renderNav(MAIN_HTML, LINKS)
+createSeparator(MAIN_HTML)
