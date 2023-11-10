d8d00304586e82baf18bd42e29a25c7092a62572
KonstancjaMG <konstancja.maciejewska-gruenig@hicoders.ch>
2023-11-10 21:34:39 +0100

    Added guidelines for setting up navigation.

---
+++ navigation.js
@@ -0,0 +1,15 @@
+// Setting up navigation:
+// 1. navbar - wrapper for the navigation.
+// 2. div class = "link" for the links.
+// 3. div id = "schoolLogo" for easy styling of the school's logo.
+// 4. div id = "schoolName" for easy styling of the school's name.
+// 5. Vertical design for more of a slick, professional look.
+// 6. Array of objects for links.
+// 7. Links:
+//      - home
+//      - students
+//      - classes
+//      - teachers
+//      - grades
+//      - announcements/notes
+//      - login (non-functional)
