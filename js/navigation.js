cefb0a00d22a7b9ba13498828681942d1d4dae0e
KonstancjaMG <konstancja.maciejewska-gruenig@hicoders.ch>
2023-11-14 17:58:29 +0100

    Changed the navigation to a template.

--- js/navigation.js
+++ js/navigation.js
@@ -1,72 +1,26 @@
-function createNavBar() {
-    const navbar = document.createElement('div');
-    navbar.id = 'navWrapper';
+import { linksArray } from "./data.js";
 
-    return navbar
-}
+const app = document.getElementById("app"); //gets the html element with the of "app"
 
-function addLogo() {
-    const logoContainer = document.createElement('div');
-    logoContainer.id = 'school-logo-container';
-    const logo = document.createElement('img');
-    logo.setAttribute('src', './images/school_logo.png');
-    logoContainer.appendChild(logo);
-    return logoContainer
-}
-
-function addSchoolName() {
-    const textParagraph = document.createElement('p');
-    textParagraph.id = 'school-name';
-    textParagraph.textContent = 'Raccoon City School District';
-    return textParagraph
-}
-
-function createLogoAndSchoolLink() {
-    const linkWrap = document.createElement('a');
-    linkWrap.id = 'school-and-logo-link-wrap'
-    linkWrap.setAttribute('href', '/home.html')
-    const logo = addLogo();
-    const schoolName = addSchoolName();
-    const logoAndSchoolContainer = document.createElement('div');
-    logoAndSchoolContainer.id = 'logo-and-school-container';
-    logoAndSchoolContainer.appendChild(logo);
-    logoAndSchoolContainer.appendChild(schoolName);
-    linkWrap.appendChild(logoAndSchoolContainer);
-
-    return linkWrap;
-}
-
-function generateLinkElements(pLinksObjects) {
-    const listOfLinks = document.createElement('ul');
-    listOfLinks.id = 'vertical-menu';
-    pLinksObjects.forEach(link => {
-        // Creates <li> elements for each item of the array of objects.
-        const listItem = document.createElement('li');
-
-        // Creates links for each item of the array of objects.
-        const anchor = document.createElement('a');
-        anchor.setAttribute('href', link.url);
-        anchor.textContent = link.name;
-        if(link.status === 'inactive') {
-            anchor.classList.add('inactive-link');
-        } else {
-            anchor.classList.add('navbar-link-item');
-        }
-        // Appends links to each list item and each list item to the parent <ul>.
-        listItem.appendChild(anchor);
-        listOfLinks.appendChild(listItem);
-    })
-    return listOfLinks
-}
-
-
-
-function renderNav(pParentElement, pLinksObjects) {
-    const navbar = createNavBar();
-    navbar.appendChild(createLogoAndSchoolLink());
-    navbar.appendChild(generateLinkElements(pLinksObjects));
-
-    pParentElement.appendChild(navbar);
-}
+const renderNav = () => {
+  app.innerHTML += `<div id="navWrapper">
+    <a id="school-and-logo-link-wrap" href="/home.html">
+    <div id="logo-and-school-container">
+        <div id="school-logo-container">
+          <img src="./images/school_logo.png" />
+        </div>
+        <p id="school-name">Raccoon City School District</p>
+      </div>
+    </a>
+    <ul id="vertical-menu">
+    ${linksArray
+      .map((link) => `
+    <li><a href="${link.url}" class="navbar-link-item">${link.name}</a></li>`).join(" ")}
+    </ul>
+    </div>
+  <div class="separator">
+  </div>`;
+};
 
 export {renderNav}
+
