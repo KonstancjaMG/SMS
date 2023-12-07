fa3ed489c907ae5673cbf712114200d219223b92
KonstancjaMG <konstancja.maciejewska-gruenig@hicoders.ch>
2023-12-07 13:56:40 +0100

    Adjusted students cards styling and added a searchbar.

--- js/content/students.js
+++ js/content/students.js
@@ -3,28 +3,32 @@ import { students } from "../data.js";
 function createStudentsContent() {
   const contentContainer = document.getElementById('contentWrapper');
   contentContainer.innerHTML = `
-    <div class="row">
-      <div class="col-md-3" id="filterColumn">
-        <div id="filters" class="border">
-          <h4>Filters</h4>
-
-        </div>
+      <div class="container" id="search-bar-container">
+      ${createSearchBar()}
       </div>
-      <div class="col-md-9 container" id="cardsColumn">
+      <div class="container" id="cardsColumn">
         ${createStudentCards(students)}
       </div>
-    </div>
   `;
+}
+
+function createSearchBar() {
+  return `
+  <div class="input-group mb-3 mt-5" id="search-bar">
+  <input type="text" class="form-control" id="students-search" placeholder="Search...">
+  <span class="input-group-text" id="basic-addon1"></span>
+</div>
+  `
 }
 
 function createStudentCards(pStudentsArray) {
   const cards = pStudentsArray.map(student => {
       return `
-        <div class="col-md-5 mb-4">
+        <div class="col-12 col-md-6 col-lg-4 mb-4">
           <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center fw-bold">
               <span>${student.name} ${student.surname}</span>
-              <span class="text-end">${student.class}</span>
+              <span class="text-end">Class: ${student.class}</span>
             </div>        
             <div class="card-body text-start">
               <p class="d-flex justify-content-between align-items-center">
@@ -44,13 +48,16 @@ function createStudentCards(pStudentsArray) {
                 <span>${student.parentsContact}</span>
               </p>
             </div>
-            <div class="card-footer text-end text-decoration-underline">Edit ></div>
+            <div class="card-footer text-end">
+              <button type="button" id="edit-btn" class="btn btn-light"><i class="fa-solid fa-pen"></i></button>
+              <button type="button" id="remove-btn" class="btn btn-light"><i class="fa-solid fa-trash"></i></button>
+            </div>
           </div>
         </div>
       `;
     }).join('');
   
-  return `<div class="row mb-5 mt-5 d-flex justify-content-center">${cards}</div>`;
+  return `<div class="row mb-5 mt-5">${cards}</div>`;
 }
 
 const renderStudents = () => {
